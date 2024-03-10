2951c4b436623661952bbbf540f9c8f47391e342
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-03-10 23:57:55 +0200

    fix(js): random bug fixes in js

--- scripts/index.js
+++ scripts/index.js
@@ -6,57 +6,11 @@ const form = document.querySelector("#form");
 const income_amt = document.querySelector("#inc-amt");
 const expense_amt = document.querySelector("#exp-amt");
 
-const localStorageTrans = JSON.parse(localStorage.getItem);
-let transactions = localStorage.getItem("trans") != null ? localStorage : [];
+const localStorageTrans = JSON.parse(localStorage.getItem("trans"));
+let transactions = localStorageTrans != null ? localStorageTrans : [];
 
 function updateLocalStorage() {
   localStorage.setItem("trans", JSON.stringify(transactions));
-}
-
-function updateAmount() {
-  const amounts = transactions.map((transaction) => transaction.amount);
-  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
-  balance.innerHTML = `$ ${total}`;
-
-  const income = amounts
-    .filter((item) => item > 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
-  income_amt.innerHTML = `$ ${income}`;
-  const expense = amounts
-    .filter((item) => item < 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
-  expense_amt.innerHTML = `$ ${Math.abs(expense)}`;
-
-
-}
-
-function addTransaction(e) {
-  e.preventDefault();
-
-  if (description.value.trim() == "" || amount.value.trim() == "") {
-    alert("Please enter description and amount of transaction!!");
-  } else {
-    const transaction = {
-      id: uniqueId,
-      description: description.value,
-      amount: +amount.value,
-    };
-  } // we need to update amounts now
-}
-
-function loadTransactionDetails(transaction) {
-  const sign = transaction.amount < 0 ? "-" : "+";
-  const item = document.createElement("li");
-  item.classList.add(transaction < 0 ? "exp" : "inc");
-  item.innerHTML = `
-    ${transaction.description}
-    <span>${sign} ${Math.abs(transaction.amount)}</span>
-    <button class="btn-del" onclick="removeTrans(${
-      transaction.id
-    })">x</button>`;
-  trans.appendChild(item);
 }
 
 function config() {
@@ -65,14 +19,63 @@ function config() {
   updateAmount();
 }
 
+function addTransaction(e) {
+  e.preventDefault();
+  if (description.value.trim() == "" || amount.value.trim() == "") {
+    alert("Please Enter Description and amount of transactions!!");
+  } else {
+    const transaction = {
+      id: uniqueId(),
+      description: description.value,
+      amount: +amount.value,
+    };
+    transactions.push(transaction);
+    loadTransactionDetails(transaction);
+    description.value = "";
+    amount.value = "";
+    updateAmount();
+    updateLocalStorage();
+  }
+}
+
 function removeTrans(id) {
-  if (confirm("Are you sure you want to delete this transaction?")) {
+  if (confirm("Are you sure you want to delete Transcation?")) {
     transactions = transactions.filter((transaction) => transaction.id != id);
     config();
     updateLocalStorage();
   } else {
     return;
   }
+}
+
+function loadTransactionDetails(transaction) {
+  const sign = transaction.amount < 0 ? "-" : "+";
+  const item = document.createElement("li");
+  item.classList.add(transaction.amount < 0 ? "exp" : "inc");
+  item.innerHTML = `
+      ${transaction.description}
+      <span>${sign} ${Math.abs(transaction.amount)}</span>
+      <button class="btn-del" onclick="removeTrans(${
+        transaction.id
+      })">x</button>`;
+  trans.appendChild(item);
+}
+
+function updateAmount() {
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  balance.innerHTML = `$  ${total}`;
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  income_amt.innerHTML = `$  ${income}`;
+  const expense = amounts
+    .filter((item) => item < 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  expense_amt.innerHTML = `$  ${Math.abs(expense)}`;
 }
 
 function uniqueId() {
